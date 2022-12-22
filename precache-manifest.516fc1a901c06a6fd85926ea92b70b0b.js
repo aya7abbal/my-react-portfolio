ff165adb97f610b98eea34666bfdaa347dcfd053
Aya El Habbal <aya7abbal@gmail.com>
2022-12-22 14:47:00 +0200

    Updates

--- precache-manifest.516fc1a901c06a6fd85926ea92b70b0b.js
+++ precache-manifest.516fc1a901c06a6fd85926ea92b70b0b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c4f580ace083ef077cfb",
+    "revision": "a5c2eba26d42a908041f",
     "url": "/my-react-portfolio/static/css/main.a09d5594.chunk.css"
   },
   {
-    "revision": "c4f580ace083ef077cfb",
-    "url": "/my-react-portfolio/static/js/main.c4f580ac.chunk.js"
+    "revision": "a5c2eba26d42a908041f",
+    "url": "/my-react-portfolio/static/js/main.a5c2eba2.chunk.js"
   },
   {
     "revision": "8468c75e08acd51e2366",
@@ -40,28 +40,28 @@ self.__precacheManifest = [
     "url": "/my-react-portfolio/static/media/me5.92dcb7be.png"
   },
   {
-    "revision": "7eb9f479e26d80e45080330f4f7112a5",
-    "url": "/my-react-portfolio/static/media/Project1.7eb9f479.png"
+    "revision": "23c312ef35f0f0506af20d9c1cecd3b6",
+    "url": "/my-react-portfolio/static/media/Project1.23c312ef.jpg"
   },
   {
-    "revision": "90b9f9c905e791214cdc6618c46cb06f",
-    "url": "/my-react-portfolio/static/media/Project2.90b9f9c9.png"
+    "revision": "2d54326fd7bb169f291c1ea481fbf41b",
+    "url": "/my-react-portfolio/static/media/Project2.2d54326f.jpg"
   },
   {
-    "revision": "1e68185e7c7b86a612d5f71db9b2f971",
-    "url": "/my-react-portfolio/static/media/Project3.1e68185e.png"
+    "revision": "080eda88b88b8e03c196514cb43ae197",
+    "url": "/my-react-portfolio/static/media/Project3.080eda88.jpg"
   },
   {
-    "revision": "644a4305b3e15fcfa75f31a7ddd3a73d",
-    "url": "/my-react-portfolio/static/media/Project4.644a4305.png"
+    "revision": "5d8d1639a27ff40eda46f85d4283fa31",
+    "url": "/my-react-portfolio/static/media/Project4.5d8d1639.jpg"
   },
   {
-    "revision": "a61f58ff3c73377b58622197fab0d186",
-    "url": "/my-react-portfolio/static/media/Project5.a61f58ff.png"
+    "revision": "40bd3c54d9b10145666288eed75051fd",
+    "url": "/my-react-portfolio/static/media/Project5.40bd3c54.jpg"
   },
   {
-    "revision": "9eb7cc44ef90249ab67b9f52f65858f3",
-    "url": "/my-react-portfolio/static/media/Project6.9eb7cc44.png"
+    "revision": "7167b65fb149c71477380e2bd7801e1b",
+    "url": "/my-react-portfolio/static/media/Project6.7167b65f.jpg"
   },
   {
     "revision": "a8ab2f987690225b22c09d7a84c4ecd1",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "/my-react-portfolio/static/media/Diamonds-bg.018bfcf8.png"
   },
   {
-    "revision": "62e08bc15b39a001e2474001ab66dc64",
+    "revision": "a734bb704fd59349aea0a4b2356b9b73",
     "url": "/my-react-portfolio/index.html"
   }
 ];
